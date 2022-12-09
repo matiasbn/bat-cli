b6f4cefd9527eabe267bc33eb0103f2ae99219fe
Matías Barrios <matias.barriosn@gmail.com>
2022-12-09 12:07:49 -0300

    updating to-review co files on templates update

--- src/commands/templates.rs
+++ src/commands/templates.rs
@@ -1,4 +1,4 @@
-use std::process::Command;
+use std::{fs, process::Command};
 
 use crate::{config::BatConfig, constants::BASE_REPOSTORY_NAME, git::clone_base_repository};
 
@@ -47,6 +47,30 @@ pub fn update_templates() {
             std::str::from_utf8(output.stderr.as_slice()).unwrap()
         )
     };
-
+    println!("Updating to-review files in code-overhaul folder");
+    // move new templates to to-review in the auditor notes folder
+    let to_review_path = BatConfig::get_auditor_code_overhaul_to_review_path(None);
+    // if the auditor to-review code overhaul folder exists
+    if fs::read_dir(to_review_path.clone()).is_ok() {
+        let to_review_folder = fs::read_dir(to_review_path).unwrap();
+        for file in to_review_folder {
+            let file_name = file.unwrap().file_name().into_string().unwrap();
+            if file_name != ".gitkeep" {
+                let file_path =
+                    BatConfig::get_auditor_code_overhaul_to_review_path(Some(file_name.clone()));
+                let template_path = BatConfig::get_code_overhaul_template_path();
+                let output = Command::new("cp")
+                    .args([template_path, file_path])
+                    .output()
+                    .unwrap();
+                if !output.stderr.is_empty() {
+                    panic!(
+                        "templates update failed with error: {:?}",
+                        std::str::from_utf8(output.stderr.as_slice()).unwrap()
+                    )
+                };
+            }
+        }
+    };
     println!("Templates folder successfully updated");
 }
