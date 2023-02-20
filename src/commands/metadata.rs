432868d4e91f76589a3a24df7372ca319799cbcb
Matías Barrios <matias.barriosn@gmail.com>
2023-02-20 05:01:04 -0300

    package: format commit

--- src/commands/metadata.rs
+++ src/commands/metadata.rs
@@ -8,7 +8,6 @@ use crate::batbelt::path::BatFile;
 use crate::{batbelt, GitCommit};
 use colored::Colorize;
 
-use crate::batbelt::metadata::BatMetadataType::Functions;
 use error_stack::{Report, Result, ResultExt};
 
 use super::CommandError;
@@ -17,24 +16,6 @@ pub fn functions() -> Result<(), CommandError> {
     let mut functions_metadata_markdown = BatMetadataType::Functions
         .get_markdown()
         .change_context(CommandError)?;
-    let is_initialized = BatMetadataType::Functions
-        .is_initialized()
-        .change_context(CommandError)?;
-    if is_initialized {
-        let user_decided_to_continue = batbelt::cli_inputs::select_yes_or_no(
-            format!(
-                "{}, are you sure you want to continue?",
-                format!("Functions section in metadata.md is already initialized").bright_red()
-            )
-            .as_str(),
-        )
-        .unwrap();
-        if !user_decided_to_continue {
-            return Err(Report::new(CommandError).attach_printable(format!(
-                "User decided not to continue with the update process for functions metadata"
-            )));
-        }
-    }
     let functions_metadata =
         FunctionMetadata::get_functions_metadata_from_program().change_context(CommandError)?;
     let functions_markdown_content = functions_metadata
@@ -57,24 +38,6 @@ pub fn functions() -> Result<(), CommandError> {
 }
 
 pub fn structs() -> Result<(), CommandError> {
-    let is_initialized = BatMetadataType::Structs
-        .is_initialized()
-        .change_context(CommandError)?;
-    if is_initialized {
-        let user_decided_to_continue = batbelt::cli_inputs::select_yes_or_no(
-            format!(
-                "{}, are you sure you want to continue?",
-                format!("Functions section in metadata.md is already initialized").bright_red()
-            )
-            .as_str(),
-        )
-        .unwrap();
-        if !user_decided_to_continue {
-            return Err(Report::new(CommandError).attach_printable(format!(
-                "User decided not to continue with the update process for structs metadata"
-            )));
-        }
-    }
     let mut structs_metadata_markdown = BatMetadataType::Structs
         .get_markdown()
         .change_context(CommandError)?;
